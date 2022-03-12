5990935dd3be26f1eae10cd78d249d7a242eddda
univrsal <uni@vrsal.xyz>
2022-03-12 17:36:40 +0100

    PreviewProgramItem: Remove #pragma from source file

--- src/preview_program_item.cpp
+++ src/preview_program_item.cpp
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *************************************************************************/
 
-#pragma once
 #include "preview_program_item.hpp"
 #include "layout.hpp"
 #include <QApplication>
