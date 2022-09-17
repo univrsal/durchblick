196cd84fe0564a2611448a5a3156a7b25746ca29
univrsal <uni@vrsal.xyz>
2022-09-17 18:27:34 +0200

    SourceItem: Remove unused colors

--- src/items/source_item.cpp
+++ src/items/source_item.cpp
@@ -214,11 +214,7 @@ void SourceItem::WriteToJson(QJsonObject& Obj)
     }
 }
 
-static const uint32_t outerColor = 0xFFD0D0D0;
 static const uint32_t labelColor = 0xD91F1F1F;
-static const uint32_t backgroundColor = 0xFF000000;
-static const uint32_t previewColor = 0xFF00D000;
-static const uint32_t programColor = 0xFFD00000;
 
 void SourceItem::Render(DurchblickItemConfig const& cfg)
 {
