60e10da48df19ebf93c8f5c7154692397c5c8589
univrsal <uni@vrsal.xyz>
2023-01-17 00:28:16 +0100

    Misc: Update headers

--- src/config.hpp
+++ src/config.hpp
@@ -1,7 +1,7 @@
 /*************************************************************************
  * This file is part of durchblick
  * git.vrsal.xyz/alex/durchblick
- * Copyright 2022 univrsal <uni@vrsal.xyz>.
+ * Copyright 2023 univrsal <uni@vrsal.xyz>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
