c62e9aed1ad25172b86ec29c5c012882732c6bf2
univrsal <uni@vrsal.xyz>
2023-05-28 20:03:00 +0200

    Misc: Update formatting script

--- format.sh
+++ format.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#grep -rl 'Copyright 2021' ./src | xargs sed -i 's/Copyright 2021/Copyright 2022/g'
+last_year=$(date -d "last year" +%Y)
+current_year=$(date +%Y)
+grep -rl 'Copyright ${last_year}' ./src | xargs sed -i 's/Copyright ${last_year}/Copyright ${current_year}/g'
 #grep -rl 'uni@vrsal.de' ./src | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
 find ./src -iname *.h* -o -iname *.c* | xargs clang-format -style=File -i -verbose
